72449e8070158c23b3c93bd2f707e9b7af9f6f1b
Ajknight121 <aknig2@uic.edu>
2025-06-02 16:41:41 -0500

    comment unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+// import NavDropdown from "react-bootstrap/NavDropdown";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
 import "./App.css";
 import { useState, useEffect } from "react";
